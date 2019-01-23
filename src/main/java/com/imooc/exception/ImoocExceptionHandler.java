fbd8672a56cd58dc73a6323042bff4657119e8a0
xiaojsp <1292541579@qq.com>
2019-01-23 18:19:20 +0800

    判断web和ajax异常捕获

--- src/main/java/com/imooc/exception/ImoocExceptionHandler.java
+++ src/main/java/com/imooc/exception/ImoocExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.imooc.exception;
 
+import com.imooc.pojo.IMoocJSONResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ImoocExceptionHandler {
     public static final String ERROR_VIEW="error";
 
-    @ExceptionHandler(value= Exception.class)
+   /* @ExceptionHandler(value= Exception.class)
     public Object errorHandler(HttpServletRequest request, HttpServletResponse response,Exception e) throws Exception{
         e.printStackTrace();
         ModelAndView mav= new ModelAndView();
@@ -19,5 +20,29 @@ public class ImoocExceptionHandler {
         mav.addObject("url",request.getRequestURL());
         mav.setViewName(ERROR_VIEW);
         return mav;
+    }*/
+    @ExceptionHandler(value= Exception.class)
+    public Object errorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception{
+        e.printStackTrace();
+        if(isAjax(request)){
+            return IMoocJSONResult.errorException(e.getMessage());
+        }else {
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("exception", e);
+            mav.addObject("url", request.getRequestURL());
+            mav.setViewName(ERROR_VIEW);
+            return mav;
+        }
+    }
+
+    /**
+     *
+     * @Title: IMoocExceptionHandler.java
+     * @Package com.imooc.exception
+     * @Description: 判断是否是ajax请求
+     *
+     */
+    private static boolean isAjax(HttpServletRequest HttpRequest) {
+        return (HttpRequest.getHeader("X-Requested-With") != null) && "XMLHttpRequest".equals(HttpRequest.getHeader("X-Requested-With").toString());
     }
 }
