0d42ac8d2563602b8826d5f2c14a03b2c149b059
xiaojsp <1292541579@qq.com>
2019-02-28 14:42:07 +0800

    事实现基于Mybatis 的CRUD功能，使用pagehelper实现分页

--- src/main/java/com/imooc/pojo/SysUser.java
+++ src/main/java/com/imooc/pojo/SysUser.java
@@ -1,7 +1,9 @@
 package com.imooc.pojo;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
-import javax.persistence.*;
 
 @Table(name = "sys_user")
 public class SysUser {
